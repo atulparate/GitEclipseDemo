cf26e60646d7f07b52774025825d3baadfcdabbd
atulparate <atulparate786@gmail.com>
2017-06-07 12:12:34 +0530

    first commit

---
+++ src/scripts/GitDemo.java
@@ -0,0 +1,11 @@
+package scripts;
+
+public class GitDemo {
+	public static void main(String args[]){
+		System.out.println("I am in main class");
+		GitTest gt = new GitTest();
+		
+		gt.test1();
+		//Done
+	}
+}
---
+++ src/scripts/GitTest.java
@@ -0,0 +1,9 @@
+package scripts;
+
+public class GitTest {
+	public void test1(){
+		
+		System.out.println("I am in GitTest");
+		
+	}
+}
